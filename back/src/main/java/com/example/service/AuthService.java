c74a5bed7474a2099f1beff7cb787eabe3b5d6cc
백승민 <seung9464@naver.com>
2025-09-04 12:28:57 +0900

    refactor: 실제 인증/회원가입 로직 주석 수정  - AuthController 추가

--- back/src/main/java/com/example/service/AuthService.java
+++ back/src/main/java/com/example/service/AuthService.java
@@ -1,3 +1,6 @@
+/**
+ * 실제 인증/회원가입 로직
+ */
 package com.example.service;
 
 import com.example.dto.SignupRequest;
@@ -17,6 +20,9 @@ public class AuthService {
         this.passwordEncoder = passwordEncoder;
     }
 
+    /**
+     * 이메일, 이름 중복 체크
+     */
     public User registerUser(SignupRequest request) {
         if (userRepository.existsByEmail(request.getEmail())) {
             throw new IllegalArgumentException("이미 사용 중인 이메일입니다.");
@@ -26,8 +32,11 @@ public class AuthService {
         }
 
         User user = new User();
+
         user.setUsername(request.getUsername());
         user.setEmail(request.getEmail());
+
+        /* 비밀번호 해시 값으로 저장 */
         user.setPassword(passwordEncoder.encode(request.getPassword()));
 
         return userRepository.save(user);
